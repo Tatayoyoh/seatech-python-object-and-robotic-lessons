6c3da5628eaf015da9591c9cac38411c9a76c796
Tatayoyoh <world.wild.bob@gmail.com>
2022-02-03 00:23:17 +0100

    add camera recognition in controller

--- 03-webots/epuck_camera_simulation/controllers/seatech_epuck_controller/SeatechEpuckRobot.py
+++ 03-webots/epuck_camera_simulation/controllers/seatech_epuck_controller/SeatechEpuckRobot.py
@@ -1,3 +1,4 @@
+from time import time
 from controller import Robot, DistanceSensor
 from EpuckMotors import EpuckMotors
 from EpuckCamera import EpuckCamera
@@ -6,35 +7,54 @@ from EpuckDistanceSensors import EpuckDistanceSensors
 
 class SeatechEpuckRobot(Robot):
 
-    def __init__(self, speed=None):
+    def __init__(self, speed=None, sampling_period=1):
         super().__init__()
         self.__motors = EpuckMotors()
-        self.__camera = EpuckCamera('camera')
+        self.__camera = EpuckCamera()
         self.__distance_sensors = EpuckDistanceSensors()
-        # self.__camera. TODO set Recognition Nodes into World
-        
-            
+        self.__goal_reached = False
+        self.__sampling_period = sampling_period
+
         self.__speed = speed
         if speed is None:
             self.__speed = self.__motors.get_max_speed()
+
+    def track_object(self, object_name):
+        self.__camera.track_object(object_name)
+
+    def __process_recognition(self):
+        self.__camera.recognize_objects()
+
+        if self.__camera.is_tracked_object_on_left():
+            self.turn_left(capacity=0.5)
+        if self.__camera.is_tracked_object_on_right():
+            self.turn_right(capacity=0.5)
 
     def run(self, backward=False, left=False, right=False):
         speed = (-self.__speed) if backward == True else self.__speed
         self.__motors.run(speed=speed, left=left, right=right)
 
-        if self.__distance_sensors.front_right_collision_detected():
-            self.__motors.turn_left()
-        if self.__distance_sensors.front_left_collision_detected():
-            self.__motors.turn_right()
+        # every 3 steps
+        if round(self.getTime() % self.__sampling_period , 1) == 0.1:
+            self.__process_recognition()
+
+        if self.__goal_reached: # TODO !!
+            print('FOUND THE OBJECT !!')
+            self.stop()
+            return False
+        elif self.__distance_sensors.front_right_collision_detected():
+            self.turn_left()
+        elif self.__distance_sensors.front_left_collision_detected():
+            self.turn_right()
 
     def stop(self, backward=False):
         self.__motors.run(speed=0)
 
-    def turn_left(self):
-        self.__motors.turn_left()
+    def turn_left(self, capacity=1):
+        self.__motors.turn_left(capacity=capacity)
 
-    def turn_right(self):
-        self.__motors.turn_right()
+    def turn_right(self, capacity=1):
+        self.__motors.turn_right(capacity=capacity)
 
     def get_recognized_objects(self):
         print(self.__camera.getRecognitionNumberOfObjects())
