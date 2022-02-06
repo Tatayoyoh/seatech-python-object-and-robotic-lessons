f3019d07366487d39c529b0c15764a52600fea63
Tatayoyoh <world.wild.bob@gmail.com>
2022-02-06 18:47:04 +0100

    add game tokens for catcher/catched robots

--- 03-webots/epuck_team_playground_simulation/controllers/seatech_team_supervisor/seatech_team_supervisor.py
+++ 03-webots/epuck_team_playground_simulation/controllers/seatech_team_supervisor/seatech_team_supervisor.py
@@ -6,7 +6,23 @@ if __name__ == '__main__':
 
     root_node = supervisor.getRoot()
     children_field = root_node.getField('children')
-    children_field.importMFNodeFromString(4, 'DEF Seatech-E-puck Robot { controller "seatech_epuck_controller" }')
+    # children_field.importMFNodeFromString(-1, 'Seatech-E-puck { translation 2.5 0 0.334, controller "seatech_epuck_controller" }')
+    children_field.importMFNodeFromString(-1, 'Seatech-E-puck { translation 1 0 0 }')
+    children_field.importMFNodeFromString(-1, 'Catcher-Token { translation 1 0 0.1 }')
+
+    children_field.importMFNodeFromString(-1, 'Seatech-E-puck { translation 0.5 0 0 }')
+    children_field.importMFNodeFromString(-1, 'Normal-Token { translation 0.5 0 0.1 }')
+
+    children_field.importMFNodeFromString(-1, 'Seatech-E-puck { translation 1 1 0 }')
+    children_field.importMFNodeFromString(-1, 'Normal-Token { translation 1 1 0.1 }')
+
+    children_field.importMFNodeFromString(-1, 'Seatech-E-puck { translation 1 0.5 0 }')
+    children_field.importMFNodeFromString(-1, 'Normal-Token { translation 1 0.5 0.1 }')
+
+    children_field.importMFNodeFromString(-1, 'Seatech-E-puck { translation 0.5 0.5 0 }')
+    children_field.importMFNodeFromString(-1, 'Normal-Token { translation 0.5 0.5 0.1 }')
+
+
 
     while supervisor.step(TIME_STEP) != -1:
         pass
